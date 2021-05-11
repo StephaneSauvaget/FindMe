81fa8263646fc35430858c81b2a8d1d08b06506e
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-05-11 23:08:37 +0200

    correct issues

--- src/Model/ApiManager.php
+++ src/Model/ApiManager.php
@@ -12,11 +12,11 @@ class ApiManager extends AbstractManager
         $response = $client->request('GET', "https://api.nasa.gov/mars-photos/api/v1/rovers
         /Opportunity/photos?sol=20&camera=PANCAM&page=2&api_key=DEMO_KEY");
         $statusCode = $response->getStatusCode();
+        $result = [];
 
         if ($statusCode === 200) {
             $content = $response->getContent();
             $content = $response->toArray();
-            $result = [];
             for ($i = 0; $i < \count($content["photos"]); $i++) {
                 $result[] = $content["photos"][$i]["img_src"];
             }
