3fb30765b0328dcc29e67085a4ec1f8aacb6b6a3
Stéphane Sauvaget <s.sauvaget41000@gmail.com>
2021-05-12 13:47:24 +0200

    correct issues

--- src/Model/ApiManager.php
+++ src/Model/ApiManager.php
@@ -9,8 +9,7 @@ class ApiManager extends AbstractManager
     public function addApi(): array
     {
         $client = HttpClient::create();
-        $response = $client->request(
-            'GET', "https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?sol=20&camera=PANCAM&page=2&api_key=DEMO_KEY");
+        $response = $client->request('GET', "https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?sol=20&camera=PANCAM&page=2&api_key=DEMO_KEY");
         $statusCode = $response->getStatusCode();
         $result = [];
 
