d38ca636931c9ea306df39b4c181834f540137b0
Orijma <remymarquentin@gmail.com>
2021-05-12 11:26:47 +0200

    add form files and verif

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: aurelwcs
+ * Date: 08/04/19
+ * Time: 18:40
+ */
+
+namespace App\Controller;
+
+use App\Model\ApiManager;
+
+class HomeController extends AbstractController
+{
+    /**
+     * Display home page
+     *
+     * @return string
+     * @throws \Twig\Error\LoaderError
+     * @throws \Twig\Error\RuntimeError
+     * @throws \Twig\Error\SyntaxError
+     */
+    public function index()
+    {
+        var_dump($_FILES);
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            //TODO Add your code here to create a new accessory
+            $errors=[];
+
+            $uploadDir = 'public/uploads/';
+            // le nom de fichier sur le serveur est ici généré à partir du nom de fichier sur le poste du client (mais d'autre stratégies de nommage sont possibles)
+            $uploadFile = $uploadDir . basename($_FILES['image']['name']);
+            // Je récupère l'extension du fichier
+            $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+            // Les extensions autorisées
+            $extensions_ok = ['jpg','jpeg','png'];
+            // Le poids max géré par PHP par défaut est de 2M
+            $maxFileSize = 1500000;
+            
+            // Je sécurise et effectue mes tests
+        
+            /****** Si l'extension est autorisée *************/
+            if( (!in_array($extension, $extensions_ok ))){
+                $errors[] = 'Veuillez sélectionner une image de type Jpg ou Jpeg ou Png !';
+            }
+        
+            /****** On vérifie si l'image existe et si le poids est autorisé en octets *************/
+            if( file_exists($_FILES['image']['tmp_name']) && filesize($_FILES['image']['tmp_name']) > $maxFileSize)
+            {
+            $errors[] = "Votre fichier doit faire moins de 1,5MB !";
+            }
+
+            var_dump($errors);            
+        }         
+        if ($errors) {
+            foreach ($errors as $error) {
+                echo '<p>' . $error . '</p>' . PHP_EOL;
+            } 
+    }
+    $apiManager = new ApiManager();
+    $pictures = $apiManager->addApi();
+    return $this->twig->render('Home/index.html.twig', ['pictures' => $pictures]);
+}
+}
---
+++ src/Model/ApiManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Model;
+
+use Symfony\Component\HttpClient\HttpClient;
+
+class ApiManager extends AbstractManager
+{
+    public function addApi(): array
+    {
+        $client = HttpClient::create();
+        $response = $client->request('GET',"https://api.nasa.gov/mars-photos/api/v1/rovers/Opportunity/photos?sol=20&camera=PANCAM&page=2&api_key=DEMO_KEY");
+        $statusCode = $response->getStatusCode();
+        $result = [];
+
+        if ($statusCode === 200) {
+            $content = $response->getContent();
+            $content = $response->toArray();
+            for ($i = 0; $i < \count($content["photos"]); $i++) {
+                $result[] = $content["photos"][$i]["img_src"];
+            }
+        }
+        return $result;
+    }
+}
